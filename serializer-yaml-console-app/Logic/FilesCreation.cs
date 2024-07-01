6803ab98ede51ce38ffb9349ab0433185a602da8
Anton Gridushko <gridushko.anton@yahoo.com>
2024-07-01 19:12:16 +0300

    Fix hyphens.

--- serializer-yaml-console-app/Logic/FilesCreation.cs
+++ serializer-yaml-console-app/Logic/FilesCreation.cs
@@ -12,8 +12,7 @@ namespace serializer_yaml_console_app.Logic
             // Сериализация в YAML и запись в файлы
             var serializer = new SerializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
-				.ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults)
-                .Build();
+				.Build();
 
             var networkConfig = configuration.GetSection("NetworkConfig").Get<NetworkConfig>();
             var metaData = configuration.GetSection("MetaData").Get<MetaData>();
--- serializer-yaml-console-app/Models/MetaData.cs
+++ serializer-yaml-console-app/Models/MetaData.cs
@@ -1,9 +1,15 @@
-﻿namespace serializer_yaml_console_app.Models
+﻿using YamlDotNet.Serialization;
+
+namespace serializer_yaml_console_app.Models
 {
     public class MetaData
     {
 		public string Dsmode { get; set; }
+
+		[YamlMember(Alias = "instance-id", ApplyNamingConventions = false)]
 		public string InstanceId { get; set; }
+
+		[YamlMember(Alias = "local-hostname", ApplyNamingConventions = false)]
 		public string LocalHostname { get; set; }
 	}
 }
